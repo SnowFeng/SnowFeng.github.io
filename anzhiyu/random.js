831ac1ca306c37b282c21e2b181e9b35a86d34a9
hgs <moqisongsong@163.com>
2024-03-07 13:36:40 +0800

    Site updated: 2024-03-07 13:36:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["register-chatgpt-account/","register-claude3-account/","register-use-wildcard/","upgrade-chatgpt-account/","wildcard-questions/"];function toRandomPost(){
+var posts=["register-chatgpt-account/","upgrade-chatgpt-account/","register-claude3-account/","register-use-wildcard/","wildcard-questions/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
